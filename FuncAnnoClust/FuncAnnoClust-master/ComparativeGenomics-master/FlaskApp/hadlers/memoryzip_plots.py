9c9809ef25579fcebdcc1ab963c7ba1e349c6a0c
DariaGI <96274232+DariaGI@users.noreply.github.com>
2025-05-27 09:41:45 +0800

    Update memoryzip_plots.py

--- FuncAnnoClust/FuncAnnoClust-master/ComparativeGenomics-master/FlaskApp/hadlers/memoryzip_plots.py
+++ FuncAnnoClust/FuncAnnoClust-master/ComparativeGenomics-master/FlaskApp/hadlers/memoryzip_plots.py
@@ -25,7 +25,6 @@ def export_plots(data, export_format):
 
         # Convert plot to image
         buffer_img = io.BytesIO()
-        print("boo_4")
         try:
             if export_format == "svg":
                 fig_bytes = exported_plot.to_image(format=export_format)  # Get SVG as bytes
@@ -35,35 +34,21 @@ def export_plots(data, export_format):
                 img_bytes = scope.transform(figure=exported_plot, format=export_format)
                 buffer_img.write(img_bytes)
             buffer_img.seek(0)  # Reset buffer position
-
         except Exception as e:
             print(f"Error converting plot '{name_plots}' to image: {e}")
             continue
 
-        # Add to dictionary
         plot_file = f"{name_plots}.{export_format}"
         plots_to_zip[plot_file] = buffer_img
-
-    print("booo11", plots_to_zip)
     return plots_to_zip
 
 
 def get_zip_buffer(data, export_format):
-
     zip_buffer = io.BytesIO()
-
-    # Export plots
-    print("boo")
     plots_to_zip = export_plots(data, export_format)
-    print("_2")
-    print(plots_to_zip)
-
-    # Create ZIP archive
     with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
         for file_name, buffer in plots_to_zip.items():
-            print("boo_3")
             buffer.seek(0)  # Reset buffer position
             zip_file.writestr(file_name, buffer.read())
-
-    zip_buffer.seek(0)  # Reset ZIP buffer position
+    zip_buffer.seek(0)  
     return zip_buffer
